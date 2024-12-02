320080d9c0ce99105a2eeced5b5d8d6703ea9e36
wasif00786 <wasifaliusmani3@gmail.com>
2024-12-02 18:24:12 +0530

    changes

--- musiclibrary.py
+++ musiclibrary.py
@@ -2,6 +2,7 @@ music = {
     "kasam": "https://www.youtube.com/watch?v=pvs0s3moFwQ",
     "fight": "https://www.youtube.com/shorts/u11XqJeal7c",
     "ben": "https://www.youtube.com/watch?v=iZVhZpXVczU",
-    "cat": "https://www.youtube.com/shorts/5WwVTuQmmkg"
+    "cat": "https://www.youtube.com/shorts/5WwVTuQmmkg",
+    "dog": "https://youtube.com/shorts/QY2scM-A-ck?si=IA02JJNfX9jh0iU3"
     
 }
